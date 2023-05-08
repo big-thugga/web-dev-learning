8442a88ff736ac9ca2bf87793edb10cc26485f75
Elton Shi <elton1@live.com.au>
2023-05-08 16:27:28 +1000

    up to request body

--- README.md
+++ README.md
@@ -8,5 +8,26 @@
   - Simple types, generic types and type parameters.
   - Optional and Union
   - pydantic is a library for data validation. FastAPI is all based on pydantic. Uses object oriented programming
+      - Has inbuilt data type validation and conversion/parsing with pydantic (Type hints are automatically enforced)
+      - Provides automatic type hints, auto completions, searching and errors (Especially for bodies)
 - [ ] Go through the Tutorial - User Guide page
+  - app instance of the FastAPI class
+  - Path operation decorator and path operation function  
+    - Path operation decorator specifies the path and the operation (HTTP request method) 
+    - Path operation function defines what happens when the path and operation specified by the decorator occurs
+  - Path parameters 
+    - Specified by the path operation decorator
+  - Query parameters
+    - Parameters in the path operation function that is not in the path
+    - Goes after the ? in the URL and is separated by & characters
+    - Query parameters are required unless there is a default value. If you just want to make it optional without a specific value, use None
+  - Request body 
+    - Since request bodies is sending data, should use POST, PUT, DELETE or PATCH
+    - Request bodies in FastAPI is built using Pydantic, and inherits from the BaseModel class. This Pydantic approach provides a whole suite of added functionality
+    - Declared in the path operation function, just like a path or query parameter
+  - Path operation functions can take multiple path and query parameters, on top of a body. If the parameter is declared in the path, it is used as a path parameter. If it is a singular type, it is used as a query parameter. If it is of type Pyndatic model, it is interpreted as a body request.
+  
+  
+
+
 - [ ] Create the front page of the game (Or the root)
--- main.py
+++ main.py
@@ -1,27 +1,84 @@
-from typing import Union
-
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Union
+from enum import Enum
 
 app = FastAPI()
 
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
-
+##### First steps #####
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+
+##### Path parameters #####
+# Predefined values for path parameters using enum
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+##### Query paramters #####
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: str, item_id: str, style: str, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if style:
+        item.update({"style": style})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+##### Body #####
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+app = FastAPI()
+
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
