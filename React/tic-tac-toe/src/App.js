94016f4a7c8cdaa148a00edd163da653748c0026
Elton Shi <elton1@live.com.au>
2023-05-22 20:46:02 +1000

    uploading tic-tac-toe tutorial

---
+++ React/tic-tac-toe/src/App.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+
+export default function Square() {
+  return (
+    <Fragment>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </Fragment>
+  );
+}
